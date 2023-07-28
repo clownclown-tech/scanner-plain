e77d110e2f732fd15c7428e901742309fa7cbb23
philipp kern <philippk8@gmail.com>
2023-07-28 03:23:57 +0200

    first commit

---
+++ basic-qrcode-reader.js
@@ -0,0 +1,25 @@
+import { BrowserQRCodeReader } from '@zxing/library';
+
+const codeReader = new BrowserQRCodeReader();
+
+codeReader
+  .decodeFromInputVideoDevice(undefined, 'video')
+  .then((result) => {
+    // process the result
+
+    document.getElementById('beep-audio').play()
+
+    return result
+
+
+
+
+
+  }).then((result) => {
+    console.log(result.text)
+    document.getElementById('result').textContent = result.text
+
+    // setTimeout(function(){window.location.replace(result.text)} , 550);
+
+  })
+  .catch(err => console.error(err));
